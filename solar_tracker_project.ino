b766d6eb992e748ae56b4371fdf68a7cad8f8c25
keresztesbeata <56408136+keresztesbeata@users.noreply.github.com>
2021-12-15 19:35:03 +0200

    Add files via upload

--- solar_tracker_project.ino
+++ solar_tracker_project.ino
@@ -6,33 +6,50 @@ Servo tilt;  // create servo object to control the pitch servo
 #define DELAY 10
 #define MIN_DIFF 30
 #define STEP_DIVIDER 30
+#define ANALOG_READ_DELAY 100
 
 int pinPhotoResistorS = A1;
 int pinPhotoResistorN = A2;
 int pinPhotoResistorE = A3;
 int pinPhotoResistorW = A0;
 
-int panAngle = 90;
-int tiltAngle = 90;
+#define MIN_TILT 5
+#define MAX_TILT 85
 
+int panPin = 2;
+int tiltPin = 4;
 
+int panAngle = 90, prevPanAngle = 0;
+int tiltAngle = 90, prevTiltAngle = 0;
 
 void setup() {
+
+  // setup pins for photoresistors
   pinMode(pinPhotoResistorN, INPUT);
   pinMode(pinPhotoResistorS, INPUT);
   pinMode(pinPhotoResistorE, INPUT);
   pinMode(pinPhotoResistorW, INPUT);
+
+  // setup Serial for debugging
   Serial.begin(9600);
-  pan.attach(2);   // attaches the servo on pin 2 to the servo object
-  tilt.attach(4);  // attaches the servo on pin 3 to the servo object
+
+  // setup pins for servo motors
+  pan.attach(panPin);   // attaches the servo on pin 2 to the servo object
+  tilt.attach(tiltPin);  // attaches the servo on pin 3 to the servo object
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // reading the photoresistor values...
   int lightIntensityS = analogRead(pinPhotoResistorS);
+  delay(ANALOG_READ_DELAY);
   int lightIntensityN = analogRead(pinPhotoResistorN);
+  delay(ANALOG_READ_DELAY);
   int lightIntensityE = analogRead(pinPhotoResistorE);
+  delay(ANALOG_READ_DELAY);
   int lightIntensityW = analogRead(pinPhotoResistorW);
+  delay(ANALOG_READ_DELAY);
+
+  // printing the photoresistor values
   Serial.println("S N E W");
   Serial.print(lightIntensityS);
   Serial.print(" ");
@@ -42,18 +59,32 @@ void loop() {
   Serial.print(" ");
   Serial.println(lightIntensityW);
 
+  // compute new angles
   int SNdirectionDiff = lightIntensityS - lightIntensityN;
-  if (abs(SNdirectionDiff) >= MIN_DIFF)
-    tiltAngle = max(0, min(90, tiltAngle + SNdirectionDiff/STEP_DIVIDER));
-
+  if (abs(SNdirectionDiff) >= MIN_DIFF) {
+    prevTiltAngle = tiltAngle;
+    tiltAngle = max(MIN_TILT, min(MAX_TILT, tiltAngle + SNdirectionDiff/STEP_DIVIDER));
+  } else {
+    prevTiltAngle = tiltAngle;
+  }
   
   int EWdirectionDiff = lightIntensityE - lightIntensityW;
-  if (abs(EWdirectionDiff) >= MIN_DIFF)
+  if (abs(EWdirectionDiff) >= MIN_DIFF) {
+    prevPanAngle = panAngle;
     panAngle = max(0, min(180, panAngle + EWdirectionDiff/STEP_DIVIDER));
+  } else {
+    prevPanAngle = panAngle;
+  }
 
-  pan.write(panAngle);
-  tilt.write(tiltAngle);
+  // move motors to the new angles, if a change occured
+  if (panAngle != prevPanAngle) {
+    pan.write(panAngle);
+  }
+  if (tiltAngle != prevTiltAngle) {
+    tilt.write(tiltAngle);
+  }
 
+  // display data for debugging purposes
   Serial.println("Intensity diffs");
   Serial.print("S-N=");
   Serial.print(SNdirectionDiff);
@@ -64,18 +95,4 @@ void loop() {
   Serial.print(panAngle);
   Serial.print("; tiltAngle=");
   Serial.println(tiltAngle);
-
-  /*Check left/right and center movement*/
-//  Serial.println("Pan Control");
-//  for(int i=90* RESOLUTION;  i<180 * RESOLUTION; i++) { pan.write((float) i / RESOLUTION); delay(DELAY); }
-//  for(int i=180* RESOLUTION;  i> 0 * RESOLUTION; i--) { pan.write((float) i / RESOLUTION); delay(DELAY); }
-//  for(int i=0* RESOLUTION;  i<90 * RESOLUTION; i++) { pan.write((float) i / RESOLUTION); delay(DELAY); }
-//  
-//  /*Check up/down and center movement*/
-//  Serial.println("Tilt Control");
-//  //for(int i=90;  i<145; i++) { tilt.write(i); delay(25); }
-//  for(int i=90 * RESOLUTION; i > 0;   i--) { tilt.write((float) i / RESOLUTION); delay(DELAY); }
-//  for(int i=0 * RESOLUTION; i < 90 * RESOLUTION; i++) { tilt.write((float) i / RESOLUTION); delay(DELAY); }
-
-  delay(1000);
 }
